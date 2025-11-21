cb1b56bd7a7746629ce81ad6b6a86b3b8393cdf3
Hoang Hung <hhungln28@gmail.com>
2025-11-21 14:56:31 +0700

    Initial project

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hệ thống hỗ trợ Tutor - HCMUT</h1>
+      <p>Đang xây dựng...</p>
+    </div>
+  );
+}
+
+export default App;
